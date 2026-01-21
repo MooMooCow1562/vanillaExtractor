20e483704bb8f634c8a2694c4d75e252974ebdfe
MooMooCow1562 <106333842+MooMooCow1562@users.noreply.github.com>
2026-01-21 13:38:56 -0600

    Partially Implemented shop and various tweaks to gameplay and visuals.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let dialogButton= document.querySelector("#close")
 const dialog = document.querySelector("#dialog")
 
 let updateInterval
@@ -8,36 +7,41 @@ let vanillaBeans = 1
 let planted = 0
 let productionRate = .025
 let rotRate = 0.9925;
-let conversionRate = 1.0/4
+let conversionRate = 1.0 / 4
 let conversionDelayAmount = 10
 let conversionDelayContainer = null
 let numNames = ["#Money", "#Extract", "#Beans", "#PlantAmount", "#PlantOutput", "#BeanRate", '#RotRate']
-let numValues = [money, vanilla, vanillaBeans, planted, productionRate*2, 0, ((1-rotRate)*100)]
+let numValues = [money, vanilla, vanillaBeans, planted, productionRate * 2, 0, ((1 - rotRate) * 100)]
 let buyButtons = document.querySelectorAll(".buyStuff")
 let saleValue = 0.99
 
-dialogButton.addEventListener("click", () => dialogToggle())
+document.querySelector("#close").addEventListener("click", () => dialogToggle())
 for (const button of buyButtons) {
-    switch (button.id.at(0)){
+    switch (button.id.at(0)) {
         case 'P':   //is a planting button
-            button.addEventListener("click",(e)=>plant(e))
+            button.addEventListener("click", (e) => plant(e))
             break
         case 'C':   //is a conversion button
-            button.addEventListener("click",(e)=>convert(e))
+            button.addEventListener("click", (e) => convert(e))
             break
         case 'S':   //is a sell button
-            button.addEventListener("click",(e)=>sell(e))
+            button.addEventListener("click", (e) => sell(e))
             break
         default:
             console.log("There's a button here that there shouldn't be! Or it's not handled!")
             console.log(button)
     }
 }
+document.querySelector("#increaseConvert")
+document.querySelector("#increaseSalePrice")
+document.querySelector("#increaseBeanRate")
+document.querySelector("#increaseConvertRate")
+document.querySelector("#decreaseDecay")
 
-function dialogToggle(){
-    if(dialog.open){
+function dialogToggle() {
+    if (dialog.open) {
         dialog.close()
-    }else{
+    } else {
         dialog.showModal()
     }
 }
@@ -69,6 +73,7 @@ function updateButtonsThatRequireBeans() {
         document.getElementById("Plant1").setAttribute("disabled", "")
     }
 }
+
 function updateButtonsThatRequireExtract() {
     if (vanilla >= 100) {
         document.getElementById("Sell100").hidden = false;
@@ -90,34 +95,34 @@ function updateButtonsThatRequireExtract() {
     }
 }
 
-function updateLoop(){
-    if(vanilla <= .25 && vanillaBeans < 1 && planted === 0){
-        document.getElementById("Message").innerHTML="Wow, I can't believe you found a way to lose the game!<br>👏👏👏👏👏👏👏👏👏👏"
+function updateLoop() {
+    if (vanilla < 1 && vanillaBeans < 1 && planted === 0) {
+        document.getElementById("Message").innerHTML = "Wow, I can't believe you found a way to lose the game!<br>👏👏👏👏👏👏👏👏👏👏"
         dialogToggle()
         clearInterval(updateInterval)
     }
-    numValues = [money, vanilla, grow(), planted, productionRate*2, 0, ((1-rotRate)*100)]
+    numValues = [money, vanilla, grow(), planted, productionRate * 2, 0, ((1 - rotRate) * 100)]
     numValues[5] = numValues[4] * numValues[3]
 
-    updateButtonsThatRequireBeans()      
+    updateButtonsThatRequireBeans()
     updateButtonsThatRequireExtract()
     for (let i = 0; i < numNames.length; i++) {
         let thing = numNames[i]
         let amount
-        if(numValues[i] < 1000000){
+        if (numValues[i] < 1000000) {
             amount = numValues[i].toFixed(2)
-        }else{
+        } else {
             amount = numValues[i].toExponential()
         }
         document.querySelector(thing).innerHTML = amount
     }
 }
 
-function plant(event){
+function plant(event) {
     document.getElementById("farm").hidden = false
     let amount = 1;
     const target = event.target.id;
-    switch (target){
+    switch (target) {
         case 'Plant1':
             //do nothing
             break
@@ -131,16 +136,17 @@ function plant(event){
             console.log("There's a plant event here that there shouldn't be! Or it's not handled!")
             amount = 0;
     }
-    if(vanillaBeans < amount){              
+    if (vanillaBeans < amount) {
         return
     }
     vanillaBeans -= amount;
     planted += amount;
 }
-function convert(event){
+
+function convert(event) {
     let amount = 1;
     const target = event.target.id;
-    switch (target){
+    switch (target) {
         case 'Convert1':
             //do nothing
             break
@@ -154,18 +160,24 @@ function convert(event){
             console.log("There's a convert event here that there shouldn't be! Or it's not handled!")
             amount = 0;
     }
-    if(conversionDelayContainer !== null || vanillaBeans < amount){
+    if (conversionDelayContainer !== null || vanillaBeans < amount) {
         return
     }
     vanillaBeans -= amount;
-    document.getElementById("ConvertingT").hidden=false;
-    conversionDelayContainer = setTimeout(()=>{vanilla += (amount * conversionRate); conversionDelayContainer = null;
-        document.getElementById("ConvertingT").hidden=true}, conversionDelayAmount * 1000 * (amount/2));
+    document.getElementById("ConvertingT").hidden = false;
+    document.getElementById('ConvertButtons').hidden = true
+    conversionDelayContainer = setTimeout(() => {
+        vanilla += (amount * conversionRate);
+        conversionDelayContainer = null;
+        document.getElementById("ConvertingT").hidden = true;
+        document.getElementById('ConvertButtons').hidden = false;
+    }, conversionDelayAmount * 1000 * (amount / 2));
 }
-function sell(event){
+
+function sell(event) {
     let amount = 1;
     const target = event.target.id;
-    switch (target){
+    switch (target) {
         case 'Sell1':
             //do nothing
             break
@@ -179,17 +191,17 @@ function sell(event){
             console.log("There's a sell event here that there shouldn't be! Or it's not handled!")
             amount = 0;
     }
-    if(vanilla < amount){
+    if (vanilla < amount) {
         return
     }
-    document.getElementById("Shop").hidden=false;
+    document.getElementById("Shop").hidden = false;
     vanilla -= amount;
-    money+= saleValue * amount;
+    money += saleValue * amount;
 }
 
-function grow(){
-    planted*=rotRate
-    if(planted < 0.01){
+function grow() {
+    planted *= rotRate
+    if (planted < 0.01) {
         planted = 0;
     }
     vanillaBeans += planted * productionRate
